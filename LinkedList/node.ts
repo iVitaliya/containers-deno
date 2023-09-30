22f0d76b985241fefada8b5115749863390f812b
iVitaliya <rankedmcacc@gmail.com>
2023-09-30 14:53:50 +0200

    Additions

--- LinkedList/node.ts
+++ LinkedList/node.ts
@@ -1,13 +1,13 @@
 class LinkedListNode<T> {
-    value: T | null;
-    previous: LinkedListNode<T> | null;
-    next: LinkedListNode<T> | null;
+  value: T | null;
+  previous: LinkedListNode<T> | null;
+  next: LinkedListNode<T> | null;
 
-    constructor(value: T) {
-        this.value = value;
-        this.previous = null;
-        this.next = null;
-    }
+  constructor(value: T) {
+    this.value = value;
+    this.previous = null;
+    this.next = null;
+  }
 }
 
 export default LinkedListNode;
